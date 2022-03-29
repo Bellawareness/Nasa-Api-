c5aea7ca23a6d882acf8b0fb0b1d1893422f35af
Bella <98971465+Bellawareness@users.noreply.github.com>
2022-03-29 15:39:30 +0000

    added image on click change

--- script.js
+++ script.js
@@ -36,3 +36,17 @@ function newDate() {
 
 }
 
+var a;
+function show_hide()
+{
+if(a==1)
+{
+document.getElementById("image").style.display="inline";
+return a=0;
+}
+else 
+{
+document.getElementById("image").style.display="none";
+return a=1;
+}
+}
